91da755a1aa23e9f846486416b413c9412c23634
dkoukib <kouki4x4@gmail.com>
2018-11-10 22:01:50 +0900

    カラー表示 カラーバーの追加

--- ColorApp/ColorApp/Module/Picker/View/PickerViewController.swift
+++ ColorApp/ColorApp/Module/Picker/View/PickerViewController.swift
@@ -8,23 +8,32 @@
 
 import UIKit
 
-class PickerViewController: UIViewController {
+final class PickerViewController: UIViewController {
+    
+    @IBOutlet private weak var colorView: UIView!
 
+    @IBOutlet weak var redSlider: UISlider!
+
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var alphaSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction private func didChangeSlider(_ sender: Any) {
+        self.changeColorView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func changeColorView() {
+        let color = UIColor(red: CGFloat(self.redSlider.value/255),
+                            green: CGFloat(self.greenSlider.value/255),
+                            blue: CGFloat(self.blueSlider.value/255),
+                            alpha: CGFloat(self.alphaSlider.value/100))
+        self.colorView.backgroundColor = color
     }
-    */
-
 }
